29a2237db32e5750ac8b25632851922e287b0de1
Idlidev <idliyout@gmail.com>
2021-12-17 12:15:58 +0530

    added sxhkd and polybar config

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -1,16 +1,12 @@
 #!/bin/sh
 
 picom --config ~/.dwm/picom/picom.conf &
-dunst &
-sxhkd -c ~/.dwm/sxhkd/sxhkdrc
 redshift -P -O 4500
+dunst &
+brightnessctl set 75
 
 ~/.fehbg
 ~/.dwm/polybar/launch.sh &
 
-while xsetroot -name "  `date`"
-do
-    sleep 1
-done &
-
+sxhkd -c ~/.dwm/sxhkd/sxhkdrc &
 dwm
