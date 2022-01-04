e290b2a549dd3c4a1d74d5dd019cdc8f525bc8e4
Idlidev <idliyout@gmail.com>
2022-01-04 19:32:26 +0530

    updates

--- .dwm/polybar/launch.sh
+++ .dwm/polybar/launch.sh
@@ -3,7 +3,5 @@
 killall -q polybar
 
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-# polybar -c ~/.config/polybar/config -rq bar &
-polybar -c ~/.dwm/polybar/dwm-polybar -rq bar &
-
+ polybar -c ~/.dwm/polybar/dwm-polybar -rq bar &
 echo "Bars launched..."
