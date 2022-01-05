8bd72dbd4873863717921e7a79988ab0724e408a
Idlidev <idliyout@gmail.com>
2022-01-05 20:37:46 +0530

    added no monocle border patch

--- dwm.c
+++ dwm.c
@@ -418,6 +418,10 @@ int applysizehints(Client *c, int *x, int *y, int *w, int *h, int interact)
 	int baseismin;
 	Monitor *m = c->mon;
 
+	// return 1 if layout is monocle
+	if (&monocle == c->mon->lt[c->mon->sellt]->arrange)
+		return 1;
+
 	/* set minimum possible */
 	*w = MAX(1, *w);
 	*h = MAX(1, *h);
@@ -1562,6 +1566,11 @@ void resizeclient(Client *c, int x, int y, int w, int h)
 	c->oldh = c->h;
 	c->h = wc.height = h;
 	wc.border_width = c->bw;
+	if ((&monocle == c->mon->lt[c->mon->sellt]->arrange) && (!c->isfloating)) {
+		wc.border_width = 0;
+		c->w = wc.width += c->bw * 2;
+		c->h = wc.height += c->bw * 2;
+	}
 	XConfigureWindow(dpy, c->win, CWX | CWY | CWWidth | CWHeight | CWBorderWidth, &wc);
 	configure(c);
 	XSync(dpy, False);
