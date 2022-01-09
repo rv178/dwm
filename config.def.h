e336f22c4316addcdf1664527079df857ce3cc1e
Idlidev <idliyout@gmail.com>
2022-01-09 15:55:44 +0530

    stuffz

--- config.def.h
+++ config.def.h
@@ -67,7 +67,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = {"dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL};
+static const char *dmenucmd[] = {"dmenu_run", NULL};
 static const char *roficmd[] = {"rofi", "-show", "drun", "-show-icons", NULL};
 static const char *termcmd[] = {"st", NULL};
 
@@ -75,6 +75,7 @@ static const char *termcmd[] = {"st", NULL};
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{MODKEY, XK_c, spawn, {.v = roficmd}},
+	{MODKEY | ShiftMask, XK_c, spawn, {.v = dmenucmd}},
 	{MODKEY, XK_Return, spawn, {.v = termcmd}},
 	{MODKEY, XK_b, togglebar, {0}},
 	{MODKEY, XK_j, focusstack, {.i = +1}},
